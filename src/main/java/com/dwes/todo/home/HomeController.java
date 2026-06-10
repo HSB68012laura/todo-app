f010ac35fb5864db78a3fe9a4fa71e94d5410153
HSB68012 <hsb68012@educastur.es>
2026-06-10 22:27:34 +0200

    creo la raiz

---
+++ src/main/java/com/dwes/todo/home/HomeController.java
@@ -0,0 +1,13 @@
+package com.dwes.todo.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/login";
+    }
+}
